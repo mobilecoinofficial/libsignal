c9795d6cffe3c2260332679510e52ab5f2f526e3
the-real-adammork <adammork@gmail.com>
2025-02-06 13:40:04 -0800

    fix: test pulling ffi zip from remote host. add packaging script
    Signed-off-by: the-real-adammork <adammork@gmail.com>

---
+++ swift/package_ffi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$(dirname "$0")
+cd $SCRIPT_DIR
+
+targets=("x86_64-apple-ios" "aarch64-apple-ios" "aarch64-apple-ios-sim")
+
+rm -rf .build
+for target in "${targets[@]}"; do
+    mkdir -p ".build/target/$target/release"
+    cp "../target/$target/release/libsignal_ffi.a" ".build/target/$target/release"
+done
+
+tar -czvf target.tar.gz -C .build target
